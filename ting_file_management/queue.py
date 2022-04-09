d84add22f267e7cfc8d2b5686a7b5f229fb7518c
Willian Prado <willian_prado@hotmail.com>
2022-04-09 17:58:39 -0300

    feat(queue): add dequeue method

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -9,7 +9,12 @@ class Queue:
         self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self._data) == 0:
+            return None
+
+        value = self._data[0]
+        del self._data[0]
+        return value
 
     def search(self, index):
         """Aqui irá sua implementação"""
