bc4ef36b6a75449680a18ed48bd2bf96dcebd2d1
Willian Prado <willian_prado@hotmail.com>
2022-04-09 22:11:16 -0300

    feat(word_search): add find_word_occurrence function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,3 +1,17 @@
+def find_word_occurrence(word, file, include_content):
+    occurrences = []
+
+    for line, content in enumerate(file["linhas_do_arquivo"]):
+        if word.lower() in content.lower():
+            if include_content:
+                occurrences.append({
+                    "linha": line+1,
+                    "conteudo": content})
+            else:
+                occurrences.append({"linha": line+1})
+    return occurrences
+
+
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
 
