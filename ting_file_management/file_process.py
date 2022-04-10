9067e047677fbd0ac47c3f8f4df3dbe20673a4c6
Willian Prado <willian_prado@hotmail.com>
2022-04-09 21:03:12 -0300

    feat(file_process): add process function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+
+    processed_file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file
+    }
+
+    if not instance.search_by_filename(path_file):
+        instance.enqueue(processed_file)
+        print(processed_file, file=sys.stdout)
 
 
 def remove(instance):
