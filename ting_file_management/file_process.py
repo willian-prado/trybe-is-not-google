d212902e00ed263fb4889f72fccd134ad796e023
Willian Prado <willian_prado@hotmail.com>
2022-04-09 21:04:19 -0300

    feat(file_process): add file_metadata function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -26,4 +26,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        selected_file = instance.search(position)
+        print(selected_file, file=sys.stdout)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
